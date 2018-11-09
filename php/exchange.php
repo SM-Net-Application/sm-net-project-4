840965541d1b4bcb48d8e3c43b3a5026e0cdef09
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-11-09 09:58:56 +0100

    Added PHP Files

---
+++ php/exchange.php
@@ -0,0 +1,23 @@
+<?php
+$response = array ();
+$jsonObjPar = file_get_contents ( 'php://input' );
+if (! empty ( $jsonObjPar )) {
+	$jsonObj = json_decode ( $jsonObjPar, true );
+	if (! empty ( $jsonObj )) {
+		if (isset ( $jsonObj ["type"] )) {
+			switch ($jsonObj ["type"]) {
+				case 1 :
+					include 'query/sp_tool_q1.php';
+					break;
+			}
+		} else {
+			$response ["status"] = 3;
+		}
+	} else {
+		$response ["status"] = 2;
+	}
+} else {
+	$response ["status"] = 1;
+}
+echo json_encode ( $response );
+?>
---
+++ php/query/config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * All database connection variables
+ */
+define ( 'DB_USER', "smnet_read" );
+define ( 'DB_PASSWORD', "_Rsv676u" );
+define ( 'DB_DATABASE', "smnetzwe_albstadtita" );
+define ( 'DB_SERVER', "web.venere.vhosting-it.com" );
+?>
