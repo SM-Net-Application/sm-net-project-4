325ae8b05e4c031997aaff5a8b1f9d89fd79b6df
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2018-11-27 15:57:26 +0100

    Project updated

--- php/query/config.php
+++ php/query/config.php
@@ -2,8 +2,8 @@
 /*
  * All database connection variables
  */
-define ( 'DB_USER', "smnet_read" );
-define ( 'DB_PASSWORD', "_Rsv676u" );
-define ( 'DB_DATABASE', "smnetzwe_albstadtita" );
-define ( 'DB_SERVER', "web.venere.vhosting-it.com" );
+define ( 'DB_USER', "" );
+define ( 'DB_PASSWORD', "" );
+define ( 'DB_DATABASE', "" );
+define ( 'DB_SERVER', "" );
 ?>
