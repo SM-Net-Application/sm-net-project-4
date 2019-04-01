1ffbb4048ae79c019dd078aa8b01b7b8cc255abd
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2019-04-01 11:54:19 +0200

    Project updated

--- src/com/sm/net/sp/model/Week.java
+++ src/com/sm/net/sp/model/Week.java
@@ -1,5 +1,7 @@
 package com.sm.net.sp.model;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.time.LocalDate;
 
 import com.sm.net.util.DateUtil;
@@ -8,12 +10,15 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Week {
 
 	private IntegerProperty week;
 	private ObjectProperty<LocalDate> from;
 	private ObjectProperty<LocalDate> to;
+	private StringProperty key;
 
 	public Week(LocalDate day) {
 		super();
@@ -21,6 +26,17 @@ public class Week {
 		week = new SimpleIntegerProperty(DateUtil.getWeekOfYears(day));
 		from = new SimpleObjectProperty<LocalDate>(DateUtil.getFirstDayOfWeek(day));
 		to = new SimpleObjectProperty<LocalDate>(DateUtil.getLastDayOfWeek(day));
+		key = new SimpleStringProperty(Week.buildKey(day));
+	}
+
+	public static String buildKey(LocalDate date) {
+
+		NumberFormat nfYear = new DecimalFormat("0000");
+		NumberFormat nfMonth = new DecimalFormat("00");
+
+		return (date != null)
+				? String.format("%s%s", nfYear.format(date.getYear()), nfMonth.format(DateUtil.getWeekOfYears(date)))
+				: "";
 	}
 
 	public final IntegerProperty weekProperty() {
@@ -57,6 +73,18 @@ public class Week {
 
 	public final void setTo(final LocalDate to) {
 		this.toProperty().set(to);
+	}
+
+	public final StringProperty keyProperty() {
+		return this.key;
+	}
+
+	public final String getKey() {
+		return this.keyProperty().get();
+	}
+
+	public final void setKey(final String key) {
+		this.keyProperty().set(key);
 	}
 
 }
