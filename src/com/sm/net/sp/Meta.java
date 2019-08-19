bbf87119d7082da74298fa20dd6bf10bd8382c06
SM-Net-Application <36441539+SM-Net-Application@users.noreply.github.com>
2019-08-19 13:02:20 +0200

    Project updated

--- src/com/sm/net/sp/Meta.java
+++ src/com/sm/net/sp/Meta.java
@@ -37,7 +37,7 @@ public class Meta {
 
 		public static final String NAME = "SupportPlanner";
 
-		public static final String VERSION = "1.0 (beta 4)";
+		public static final String VERSION = "1.0 (beta 5)";
 
 		public static String getFullTitle() {
 			return DEVELOPER + ": " + NAME + " " + VERSION;
