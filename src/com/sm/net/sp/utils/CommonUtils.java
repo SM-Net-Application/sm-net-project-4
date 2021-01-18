88e7d2406cef72c63296ac4aae89eb557a4fedab
SM-Net <salv.molaro@gmail.com>
2021-01-18 17:55:33 +0100

    1.1 Project updated

--- src/com/sm/net/sp/utils/CommonUtils.java
+++ src/com/sm/net/sp/utils/CommonUtils.java
@@ -3,24 +3,31 @@ package com.sm.net.sp.utils;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class CommonUtils {
 
 	public static BigDecimal calculateAge(LocalDate startDate) {
 
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
-
 		LocalDate now = LocalDate.now();
-		BigDecimal nowBd = new BigDecimal(dtf.format(now));
 
-		BigDecimal startDateBd = new BigDecimal(dtf.format(startDate));
+		long daysBetween = ChronoUnit.DAYS.between(startDate, now);
+		BigDecimal days = new BigDecimal(daysBetween);
+		
+		BigDecimal daysYear = new BigDecimal(365.25);
+		BigDecimal age = days.divide(daysYear, 2, RoundingMode.HALF_UP);
+		
+//		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
 
-		BigDecimal age = nowBd.subtract(startDateBd);
-		age = age.divide(BigDecimal.valueOf(10000), 2, RoundingMode.HALF_DOWN);
-		BigDecimal decimal = age.remainder(BigDecimal.ONE);
-		decimal = decimal.multiply(BigDecimal.TEN);
-		age = BigDecimal.valueOf(age.intValue()).add(decimal);
+//		BigDecimal nowBd = new BigDecimal(dtf.format(now));
+//
+//		BigDecimal startDateBd = new BigDecimal(dtf.format(startDate));
+//
+//		BigDecimal age = nowBd.subtract(startDateBd);
+//		age = age.divide(BigDecimal.valueOf(10000), 2, RoundingMode.HALF_DOWN);
+//		BigDecimal decimal = age.remainder(BigDecimal.ONE);
+//		decimal = decimal.multiply(BigDecimal.TEN);
+//		age = BigDecimal.valueOf(age.intValue()).add(decimal);
 
 		return age;
 	}
